ab5ff96df954b4392f252aaf4ab617c194cee341
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-12-07 16:40:40 +0000

    Update vectores.js

--- js/vectores.js
+++ js/vectores.js
@@ -1,22 +1,52 @@
 // Vectores
-const VECTOR = [3, "plátanos", "5", "Héctor", "Plátanos", 4.5, 4, "hector", 2];
+let VECTOR = [];
+
+function validar_vec() {
+  const reg = /((\w)+(,)*)+/;
+  let vector_ayuda = document.getElementById("vector_ayuda");
+  let vector = document.getElementById("vector").value;
+  if (vector == "") {
+    vector_ayuda.innerText = "> Debes escribir una lista de palabras";
+    borrar_ayuda(vector_ayuda);
+    return;
+  }
+  let v = vector.split(" ").join("");
+  if (reg.exec(v) !== null) {
+    VECTOR = v.split(",").filter(e => e != "");
+    return true;
+  } else {
+    vector_ayuda.innerText =
+      "> Las palabras deben estar separada por coma( , )";
+    borrar_ayuda(vector_ayuda);
+    return false;
+  }
+}
 
 function ver() {
+  if (validar_vec())
+    document.getElementById("text_area").value = VECTOR.toString();
+}
+
+function ver_a() {
   document.getElementById("text_area").value = VECTOR.toString();
 }
 
-function ver_a(array) {
-  document.getElementById("text_area").value = array.toString();
+function vector_ordenado() {
+  if (validar_vec()) {
+    let numeros = VECTOR.filter((n) => typeof n === "number").sort(
+      (a, b) => a - b
+    );
+    let letras = VECTOR.filter((n) => typeof n === "string").sort(
+      Intl.Collator().compare // Ignora mayúsculas y minúsculas
+    );
+    return numeros.concat(letras);
+  }
 }
-
-vector_ordenado = () => {
-  let numeros = VECTOR.filter((n) => typeof n === "number").sort(
-    (a, b) => a - b
-  );
-  let letras = VECTOR.filter((n) => typeof n === "string").sort(
-    Intl.Collator().compare // Ignora mayúsculas y minúsculas
-  );
-  return numeros.concat(letras);
-};
-ordenar = () => ver_a(vector_ordenado());
-reves = () => ver_a(vector_ordenado().reverse());
+function ordenar() {
+  VECTOR = vector_ordenado();
+  ver_a();
+}
+function reves() {
+  VECTOR = vector_ordenado().reverse();
+  ver_a();
+}
